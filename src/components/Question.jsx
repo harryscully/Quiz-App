da53ac3fd984b11baf4d4ca14b46f53c2617fc68
harryscully <harry.scully@ugogroup.co.uk>
2026-01-27 14:59:36 +0000

    Add styling to selected answer

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -4,15 +4,16 @@ import { arrayShuffle } from "array-shuffle"
 
 export default function Question(props) {
 
-    const [shuffledAnswers, setShuffledAnswers] = useState(() => arrayShuffle([...props.data.incorrect_answers,props.data.correct_answer]))
+    const [shuffledAnswers, setShuffledAnswers] = useState(() => arrayShuffle([...props.data.incorrect_answers, props.data.correct_answer]))
 
-     const answers = shuffledAnswers.map((a,index) => {
-          return (
+    const answers = shuffledAnswers.map((a, index) => {
+        return (
             <li key={index}>
-              <input type="radio" name={`q${props.qIndex}`} id={`q${props.qIndex}-a${index}`} />
-              <label htmlFor={`q${props.qIndex}-a${index}`}>{he.decode(a)}</label>
-            </li>)
-        })
+                <input type="radio" name={`q${props.qIndex}`} id={`q${props.qIndex}-a${index}`} />
+                <label htmlFor={`q${props.qIndex}-a${index}`}>{he.decode(a)}</label>
+            </li>
+        )
+    })
 
     return (
         <div className="question">
