b02188b4c8c7558ec29244b3b33556c634010519
harryscully <harry.scully@ugogroup.co.uk>
2026-01-27 12:52:33 +0000

    Apply styling to start screen

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 
 export default function App() {
   return (
-    <section className="start-screen">
-      <h1>Quizzical</h1>
-      <p>See how you fare against 5 multiple choice trivia questions!</p>
-      <button>
-        Start quiz
-      </button>
-    </section>
+    <main>
+      <section className="start-screen">
+        <h1>Quizzical</h1>
+        <p>See how you fare against 5 multiple choice trivia questions!</p>
+        <button>
+          Start quiz
+        </button>
+      </section>
+    </main>
   )
 }
